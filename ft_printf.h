f8019797ca9cc493fb856c26c896c210e123de96
SA-Dark-LAH <13futureisloading37@gmail.com>
2024-12-21 00:42:23 +0000

    dd

--- ft_printf.h
+++ ft_printf.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdint.h>
 
 // Flag structure to store '#', '+', and ' ' (space) flags
 typedef struct s_flags {
@@ -24,6 +25,7 @@ int handle_string(va_list args);
 int handle_integer(va_list args, t_flags flags);
 int handle_unsigned(va_list args);
 int handle_hex(va_list args, t_flags flags, int uppercase);
+int handle_ptr(va_list args);
 
 // Helper functions
 int ft_putchar(char c);
@@ -34,6 +36,12 @@ int ft_putnbr(long n);
 int			hex_len(unsigned int num);
 void	ft_put_hex(unsigned int num, int uppercase);
 int	ft_puthex(unsigned int num, int uppercase);
+
+//ptr
+int	len_ptr(uintptr_t nb);
+void	ft_put_ptr(uintptr_t nb);
+int	ft_putptr(uintptr_t ptr);
+
 
 
 #endif
